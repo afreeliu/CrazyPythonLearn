be304817b6a705321960a432ffdcb8bd08bb0e85
afreeliu <afree_liu@163.com>
2019-10-18 19:03:33 +0800

    cc

--- main/Chapter/15_网络编程/15.2.3使用urllib.request读取资源.py
+++ main/Chapter/15_网络编程/15.2.3使用urllib.request读取资源.py
@@ -78,3 +78,105 @@ class DownUtil:
         self.thread_num = thread_num
         # 指定所下载的文件保存的位置
         self.target_file = target_file
+        #初始化 threads 数组
+        self.threads = []
+
+    def download(self):
+        # 创建 Request 对象
+        req = Request(url=self.path, method='GET')
+        # 添加头请求
+        req.add_header('Accept', '*/*')
+        req.add_header('Charset', 'UTF-8')
+        req.add_header('Connection', 'Keep-Alive')
+        # 打开要下载的资源
+        f = urlopen(req)
+        print(f.headers)
+        # 获取要下载的文件大小
+        self.file_size = int(dict(f.headers).get('Content-Length', 0))
+        f.close
+        # 计算每个线程要下载的资源大小
+        current_part_size = self.file_size // self.thread_num + 1
+        for i in range(self.thread_num):
+            # 计算每个线程下载的开始位置
+            start_pos = i * current_part_size
+            # 每个线程都使用一个以 wb 模式打开的文件进行下载
+            t = open(self.target_file, 'wb')
+            # 定位该线程下载的位置
+            t.seek(start_pos, 0)
+            # 创建下载线程
+            td = DownThread(self.path, start_pos, current_part_size, t)
+            self.threads.append(td)
+            # 启动下载线程
+            td.start()
+    # 获取完成的百分比
+    def get_complete_rate(self):
+        # 统计多个线程已经下载的资源总大小
+        sum_size = 0
+        for i in range(self.thread_num):
+            sum_size += self.threads[i].length
+        # 返回已经完成的百分比
+        return sum_size / self.file_size
+
+class DownThread(threading.Thread):
+    def __init__(self, path, start_pos, current_part_size, current_part):
+        super().__init__()
+        self.path = path
+        # 当前线程的下载位置
+        self.start_pos = start_pos
+        # 定义当前线程负责下载的文件大小
+        self.current_part_size = current_part_size
+        # 当前线程需要下载的文件块
+        self.current_part = current_part
+        # 定义该线程已下载的字节数
+        self.length = 0
+    def run(self):
+        # 创建 Request 对象
+        req = Request(url=self.path, method='GET')
+        # 添加请求头
+        req.add_header('Accept', '*/*')
+        req.add_header('Charset', 'UTF-8')
+        req.add_header('Connection', 'Keep-Alive')
+        # 打开要下载的资源
+        f = urlopen(req)
+        # 跳过 self.start_pos 个字节，表明该线程只下载自己负责的那部分内容
+        for i in range(self.start_pos):
+            f.read(1)
+        # 读取网络数据，并写入本地文件中
+        while self.length < self.current_part_size:
+            data = f.read(1024)
+            if data is None or len(data) <= 0:
+                break
+            self.current_part.write(data)
+            # 累计该线程下载的资源总大小
+            self.length += len(data)
+        self.current_part.close()
+        f.close
+
+'''
+     上面程序中定义了 DownThread 线程类，该线程类负责读取从 start_pos 开始、长度为 current_past_size 的所有字节数据，并写入本地文件
+     对象中。DownThread 线程类的 run() 方法就是一个简单的输入/输出实现。
+     程序中 DownUtils 类的 download() 方法负责按如下步骤来实现多线程下载
+     1 使用 urlopen() 方法打开远程资源
+     2 获取指定的 URL 对象所指向资源的大小（通过 Content-Length 响应头获取）。
+     3 计算每个线程应该下载网络资源的哪个部分（从哪个字节开始， 到哪个字节结束）
+     4 依次创建并启动多个线程来下载网络资源的指定部分
+'''
+aurl = 'https://image.baidu.com/search/down?tn=download&amp;word=download&amp;ie=utf8&amp;fr=detail&amp;url=https%3A%2F%2Ftimgsa.baidu.com%2Ftimg%3Fimage%26quality%3D80%26size%3Db9999_10000%26sec%3D1571371702433%26di%3Dfeac361f470c5bca2363a816e9068a02%26imgtype%3D0%26src%3Dhttp%253A%252F%252Fb-ssl.duitang.com%252Fuploads%252Fitem%252F201208%252F30%252F20120830031623_Ys2LQ.jpeg&amp;thumburl=https%3A%2F%2Fss1.bdstatic.com%2F70cFuXSh_Q1YnxGkpoWK1HF6hhy%2Fit%2Fu%3D1491992360%2C2030420109%26fm%3D26%26gp%3D0.jpg'
+du = DownUtil(aurl, 'a.png', 3)
+du.download()
+def show_process():
+    print('已完成 %.2f' % du.get_complete_rate())
+    global t
+    if du.get_complete_rate() < 1:
+        # 通过定时器启动 0.1s 之后执行 show_process 函数
+        t = threading.Timer(0.1, show_process)
+        t.start()
+t = threading.Timer(0.1, show_process)
+t.start()
+
+
+
+
+
+
+
